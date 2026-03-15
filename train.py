89dd57aa9235e49c0df355b1a87650bafdf233ca
Kendrej <jendrekaugustyniak@gmail.com>
2026-03-15 09:32:59 +0100

    feat: checkpoints

--- train.py
+++ train.py
@@ -1,13 +1,16 @@
 from stable_baselines3 import PPO
-from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.callbacks import BaseCallback, CheckpointCallback, CallbackList
+from collections import deque
 from env.roguelike_env import RogueLikeEnv
 import numpy as np
 import os
+import glob
+from gymnasium.wrappers import TimeLimit
 
 class RewardLogCallback(BaseCallback):
     def __init__(self):
         super().__init__()
-        self.step_rewards = []
+        self.step_rewards = deque(maxlen=1024)
         self.episode_reward_sum = 0
     
     def _on_step(self):
@@ -16,8 +19,7 @@ class RewardLogCallback(BaseCallback):
         self.episode_reward_sum += reward
         
         if self.num_timesteps % 1024 == 0:
-            self.logger.record("custom/reward_mean_1024", np.mean(self.step_rewards[-1024:]))
-            self.logger.record("custom/current_episode_sum", self.episode_reward_sum)
+            self.logger.record("custom/reward_mean_1024", np.mean(self.step_rewards))
         
         if self.locals['dones'][0]:
             self.logger.record("custom/episode_reward_sum", self.episode_reward_sum)
@@ -25,16 +27,30 @@ class RewardLogCallback(BaseCallback):
             
         return True
 
-env = RogueLikeEnv()
+base_env = RogueLikeEnv()
+env = TimeLimit(base_env, max_episode_steps=3000)
 
-if os.path.exists("checkpoints/roguelike_ppo.zip"):
-    model = PPO.load("checkpoints/roguelike_ppo", env=env)
+all_files = glob.glob("checkpoints/*.zip")
+newest_file = None
+
+if all_files:
+    newest_file = max(all_files, key=os.path.getctime)
+    print(f"Znaleziono zapisany model: {newest_file}")
+
+if newest_file:
+    model = PPO.load(newest_file, env=env)
     print("Kontynuuję trening od zapisanego modelu")
 else:
-    model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="./logs/", n_steps=1024, ent_coef=0.01)
+    model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="./logs/", n_steps=3072, ent_coef=0.02)
     print("Nowy model od zera")
 
-callback = RewardLogCallback()
-model.learn(total_timesteps=100000, callback=callback)
+log_callback = RewardLogCallback()
+
+checkpoint_callback = CheckpointCallback(save_freq=100_000, save_path="./checkpoints/", name_prefix="roguelike_ppo", verbose=1)
+
+callback_list = CallbackList([log_callback, checkpoint_callback])
+
+model.learn(total_timesteps=5_000_000, callback = callback_list)
+
 model.save("checkpoints/roguelike_ppo")
 env.close()
